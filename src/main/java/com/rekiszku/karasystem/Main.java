33570fdc0274fe5e8fc24960e54a8f83176d3805
testestsasd <ab9selo@gmail.com>
2025-07-24 02:25:08 +0200

    Update Main.java

--- src/main/java/com/rekiszku/karasystem/Main.java
+++ src/main/java/com/rekiszku/karasystem/Main.java
@@ -1,17 +1,23 @@
-package com.rekiszku.karasystem;
+package com.twojnick.karasystem;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+    private KaraManager karaManager;
+
     @Override
     public void onEnable() {
+        this.karaManager = new KaraManager(this);
         getLogger().info("KaraSystem plugin enabled!");
-        this.getCommand("kara").setExecutor(new KaraCommand(this));
+        this.getCommand("kara").setExecutor(new KaraCommand(this, karaManager));
+        this.getServer().getPluginManager().registerEvents(new GUIListener(karaManager), this);
+        karaManager.loadData();
     }
 
     @Override
     public void onDisable() {
+        karaManager.saveData();
         getLogger().info("KaraSystem plugin disabled!");
     }
 }
