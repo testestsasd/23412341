286db3d6a1f63fd12beaacf03785928a47b68f02
testestsasd <ab9selo@gmail.com>
2025-07-24 02:23:07 +0200

    Update Main.java

--- src/main/java/com/rekiszku/karasystem/Main.java
+++ src/main/java/com/rekiszku/karasystem/Main.java
@@ -1,1 +1,17 @@
+package com.rekiszku.karasystem;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("KaraSystem plugin enabled!");
+        this.getCommand("kara").setExecutor(new KaraCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("KaraSystem plugin disabled!");
+    }
+}
